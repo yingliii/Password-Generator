266f8cfbef12e02dc32be21820a4add5fe5fe160
Ying Li <lliying1213@gmail.com>
2021-06-23 18:05:10 -0400

    updated JavaScript

--- script.js
+++ script.js
@@ -1,69 +1,63 @@
 // Given: assignment Code
 var generateBtn = document.querySelector("#generate"); //to start the generating process from html generate button
-  //Assign var
-var upperCaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; //list of all upper case characters
-var lowerCaseChar = "abcdefghijklmnopqrstuvwxyz"; //list of all lower case characters
-var numChar = "1234567890"; //list of all numbers
-var specialChar = "~!@#$%^&*`{}[]|:;'<>?,./"; //list of special charaters
 
-//Write function to generate random password
-var generatePassword = function () {
-  // ask for choice of password length
-  var passwordLength = window.prompt("How many characters would you want for your password? Password lenght must be between 8-128 characters");
-  
-  //if input is less than 8, it is invalid enter
-  if (passwordLength < 8) {
-    
-    // TODO show the user input on the alert window
-    window.alert("Invalid number, please enter again. Password must have more than 7 characters.");
-    return;
-  
-  //if input is greater than 128, it is invalid enter
-  } if (passwordLength > 128) {
-    
-    // TODO show the user input on the alert window
-    window.alert("Invalid number, please enter again. Password must have less than 128 characters.");
-    return;
-  }
+//list variables
+var upperCaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerCaseChar = "abcdefghijklmnopqrstuvwxyz";
+var numChar = "1234567890";
+var specialChar = "~!@#$%^&*`{}[]|:;'<>?,./";
 
-// ask for choice of uppercase characters
-  var upperCaseChoice = window.confirm("Do you want password include UPPERCASE characters?");
-  // TODO
-  if (!upperCaseChoice) {
-    upperCaseChoice = false;
-    // ASK return here?
-  }
+function generatePassword(){
 
-// ask for choice of lowercase characters
-  var lowerCaseChoice = window.confirm("Do you want password include LOWERCASE characters?");
-  // TODO
-  if (!lowerCaseChoice) {
-    lowerCaseChoice = false;
-    // ASK return here?
-  }
+    // set up promts
+    var getLength = window.prompt("Write the length of the password. (range of 8-128 characters");
+    if (getLength < 8 || getLength > 128) {
+        window.alert("Invalid enter. Please enter number from 8-128!");
+        return;
+    }
 
-// ask for choice of numeric characters
-  var numChoice = window.confirm("Do you want password include NUMERICAL characters?");
-  // TODO
-  if (!numChoice) {
-    numChoice = false;
-    // ASK return here?
-  }
+    var selected = "";
+    var includeUpper = window.confirm("Do you want to include UPPERCASES?");
+    var includeLower = window.confirm("Do you want to include LOWERCASES?");
+    var includenumber = window.confirm("Do you want to include NUMBERS?");
+    var includesymbol = window.confirm("Do you want to include SYMBOLS?");
 
-//ask for choice of special characters
-  var specialCharChoice = window.confirm("Do you want password include SPECIAL characters?");
-  // TODO
-  if (!specialCharChoice) {
-    specialCharChoice = false;
-    // ASK return here?
-  }
-// Assign random var
-  var upperCaseRandom = String.upperCaseChar(Math.floor(Math.random() * upperCaseChar.length));
-  var lowerCaseRandom = String.upperCaseChar(Math.floor(Math.random() * lowerCaseChar.length));
-  var numRandom = String.upperCaseChar(Math.floor(Math.random() * numChar.length));
-  var specialCharRandom = String.upperCaseChar(Math.floor(Math.random() * specialChar.length));
+    if (includeUpper == true) {
+        selected += upperCaseChar;
+    }
+    if (includeLower == true) {
+        selected += lowerCaseChar;
+    }
+    if (includenumber == true) {
+        selected += numChar;
+    }
+    if (includesymbol == true) {
+        selected += specialChar;
+    }
+    console.log(selected); // the select var with confirm window works !
 
+    var getPassword = "";
+    //get password length -> parseInt is used to convert it in number
+    var passwordLength = parseInt(getLength);
+
+    console.log(passwordLength);
+
+    // create for loop to get the length
+    for(var i=0; i < passwordLength; i++) {
+        var random = Math.floor(Math.random() * selected.length);
+        getPassword = getPassword + selected[random];
+    }
+
+    console.log(getPassword);
+
+    return getPassword;
 }
+
+
+
+
+
+
 
 // Given: write password to the #password input
 function writePassword() {
@@ -73,6 +67,9 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
+
+
 
 // Given: add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
